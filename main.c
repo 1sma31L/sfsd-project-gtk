e8991b7fe68db94fd11a72272f8f005baee01015
WALIDHM2005 <W.waliduniveristy@gmail.com>
2025-01-07 14:36:39 +0100

    new one

--- main.c
+++ main.c
@@ -95,7 +95,7 @@ static void activate(GtkApplication *app, gpointer user_data)
     reorganize_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
 
     // add the boxes to the main grid
-    gtk_grid_attach(GTK_GRID(grid), title_box, 0, 0, 3, 2);
+    gtk_grid_attach(GTK_GRID(grid), title_box, 0, 0, 3, 1);
     gtk_grid_attach(GTK_GRID(grid), add_modify_box, 0, 1, 3, 2);
     gtk_grid_attach(GTK_GRID(grid), search_delete_box, 0, 3, 3, 2);
     gtk_grid_attach(GTK_GRID(grid), extract_box, 0, 5, 3, 1);
