7e6b2e45a250e1e91ff6fff8c205d46a02d9d489
WALIDHM2005 <W.waliduniveristy@gmail.com>
2025-01-07 14:11:24 +0100

    last vesrion

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include "functions.h"
 #include <gtk/gtk.h>
 
-
 static void add_student_clicked(GtkButton *button, gpointer user_data) {
     FILE *file = fopen(FILE_NAME, "a+");
 
@@ -49,7 +48,16 @@ static void activate(GtkApplication *app, gpointer user_data) {
     window = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(window), "STUDENT MANAGEMENT SYSTEM");
     gtk_window_set_default_size(GTK_WINDOW(window), 1000, 800);
+    // Get screen dimensions using the newer GdkDisplay API
+    GdkDisplay *display = gdk_display_get_default();
+    GdkMonitor *monitor = gdk_display_get_primary_monitor(display); // Primary monitor
+    GdkRectangle monitor_geometry;
+    gdk_monitor_get_geometry(monitor, &monitor_geometry);
+    gint screen_width = monitor_geometry.width;
+    gint screen_height = monitor_geometry.height;
 
+    // Set the window size to adapt to screen size
+    gtk_window_set_default_size(GTK_WINDOW(window), screen_width * 0.8, screen_height * 0.8); // 80% of screen size
     // creat a grid;
     grid = gtk_grid_new();
     gtk_widget_set_margin_top(grid, 40);
@@ -59,33 +67,35 @@ static void activate(GtkApplication *app, gpointer user_data) {
     gtk_container_add(GTK_CONTAINER(window), grid);
 
     // creat the boxes
-
+    GtkWidget *title_box=gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 20);
+    GtkWidget *label = gtk_label_new("Welcome To SMS");
+    gtk_container_add(GTK_CONTAINER(title_box), label);
 /// the first box:
-    add_modify_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 30);
+    add_modify_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 20);
     GtkWidget *grid1 = gtk_grid_new();
     gtk_container_add(GTK_CONTAINER(add_modify_box), grid1);
     gtk_grid_set_row_spacing(GTK_GRID(grid1), 20); 
-    gtk_grid_set_column_spacing(GTK_GRID(grid1), 50); 
+    gtk_grid_set_column_spacing(GTK_GRID(grid1), 30); 
 
 /// the second box
-    search_delete_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 30);
+    search_delete_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 20);
     GtkWidget *grid2 = gtk_grid_new();
     gtk_container_add(GTK_CONTAINER(search_delete_box), grid2);
     gtk_grid_set_row_spacing(GTK_GRID(grid2), 20); 
-    gtk_grid_set_column_spacing(GTK_GRID(grid2), 50); 
+    gtk_grid_set_column_spacing(GTK_GRID(grid2), 30); 
 
 /// the third box
     extract_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
     GtkWidget *grid3 = gtk_grid_new();
     gtk_container_add(GTK_CONTAINER(extract_box), grid3);
     gtk_grid_set_row_spacing(GTK_GRID(grid3), 20); 
-    gtk_grid_set_column_spacing(GTK_GRID(grid3), 50);
+    gtk_grid_set_column_spacing(GTK_GRID(grid3), 30);
 
 /// the fourth box
     reorganize_box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
 
     // add the boxes to the main grid 
-    gtk_grid_attach(GTK_GRID(grid), title_box, 0, 0, 3, 2);
+    gtk_grid_attach(GTK_GRID(grid), title_box, 0, 0, 3, 1);
     gtk_grid_attach(GTK_GRID(grid), add_modify_box, 0, 1, 3, 2);
     gtk_grid_attach(GTK_GRID(grid), search_delete_box, 0, 3, 3, 2);
     gtk_grid_attach(GTK_GRID(grid), extract_box, 0, 5, 3, 1);
@@ -211,7 +221,6 @@ static void activate(GtkApplication *app, gpointer user_data) {
 
     g_signal_connect(add_button, "clicked", G_CALLBACK(add_student_clicked), widgets);
 
-    gtk_window_maximize(GTK_WINDOW(window));
     gtk_widget_show_all(window);
 
 }
